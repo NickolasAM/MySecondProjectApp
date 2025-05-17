c6092665112c0dcc2a0823d9ede27a7e4085e6fc
Nicko Marquez <NickolasMarquez>
2025-05-17 10:49:44 -0600

    GreetingsApp

---
+++ GreetingApp/Program.cs
@@ -0,0 +1,23 @@
+﻿
+//Welcome User To App
+Console.WriteLine("Welcome to the Greeting Application");
+Console.WriteLine("This application was built by Nicko Marquez");
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine();
+
+
+//Ask for First Name
+Console.WriteLine("What is your full name: ");
+string firstName;                       //camel casing (first letter lowercse, industry standard way at appraoching VARIABLE    `  naming
+
+firstName = Console.ReadLine();
+
+string lastName;
+lastName = Console.ReadLine();
+
+//Greet user by name
+Console.WriteLine();
+Console.WriteLine("Hello " + firstName + "I see your last name is" + lastName);
+
+Console.WriteLine("Thank you for using my application.");
+Console.ReadLine();
